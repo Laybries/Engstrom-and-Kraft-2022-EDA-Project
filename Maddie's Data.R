145ff660c667e0271f17bb41d1169409f0f6fecf
Madison Kraft <fo8155tp@mnstate.edu>
2022-03-22 13:16:48 -0500

    3/22

--- Maddie's Data.R
+++ Maddie's Data.R
@@ -10,4 +10,16 @@ Oceanic_Ray <- get_inat_obs(query = "Oceanic Manta Ray",maxresults = 1000) %>%
     day = substr(time_observed_at,9,10)) %>% 
   print()
 
-ggplot(data = Oceanic_Ray, mapping = aes(latitude,longitude))
+Reef_Ray <- get_inat_obs(query = "Reef Manta Ray",maxresults = 1000) %>% 
+  select(scientific_name, time_observed_at, latitude, longitude) %>% 
+  filter(scientific_name == "Mobula alfredi",time_observed_at != "") %>% 
+  mutate(
+    year = substr(time_observed_at,0,4),
+    month = substr(time_observed_at,6,7),
+    day = substr(time_observed_at,9,10)) %>% 
+  print()
+
+Combined_Table <- 
+
+ggplot(data = Oceanic_Ray) + 
+  geom_point(mapping = aes(x = latitude, y = longitude, alpha = 0.1))
