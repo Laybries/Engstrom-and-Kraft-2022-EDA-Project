4b296e440a395eef5bedf7fdd52c90ec3e9fcced
Madison Kraft <fo8155tp@mnstate.edu>
2022-03-24 13:16:15 -0500

    3/24

--- Maddie's Data.R
+++ Maddie's Data.R
@@ -16,10 +16,26 @@ Reef_Ray <- get_inat_obs(query = "Reef Manta Ray",maxresults = 1000) %>%
   mutate(
     year = substr(time_observed_at,0,4),
     month = substr(time_observed_at,6,7),
-    day = substr(time_observed_at,9,10)) %>% 
+    day = substr(time_observed_at,9,10),
+    season = (indx[as.character(month)]))%>% 
   print()
 
-Combined_Table <- 
+Combined_Table <- rbind(Oceanic_Ray,Reef_Ray) %>% 
+  select(scientific_name, latitude, longitude, year, month,day) %>% 
+  print()
 
-ggplot(data = Oceanic_Ray) + 
-  geom_point(mapping = aes(x = latitude, y = longitude, alpha = 0.1))
+world <- map_data("world")
+
+ggplot() +
+  geom_map(
+    data = world, map = world,
+    aes(long, lat, map_id = region)
+  ) +
+  geom_point(data = Reef_Ray,
+    mapping = aes(x = longitude, y = latitude,
+      color = season,
+      size = ".01",
+      alpha = .01))
+
+indx <- setNames( rep(c('winter', 'spring', 'summer',
+                        'fall'),each=3), c("12","01","02","03","04","05","06","07","08","09","10","11"))
