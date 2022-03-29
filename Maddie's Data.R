32bd09736acdce0f034e91701855b1d476fd344c
Madison Kraft <fo8155tp@mnstate.edu>
2022-03-29 12:58:19 -0500

    3/29 finished?

--- Maddie's Data.R
+++ Maddie's Data.R
@@ -1,13 +1,17 @@
 library(rinat)
 library(tidyverse)
 
+indx <- setNames( rep(c('1:Winter', '2:Spring', '3:Summer',
+                        '4:Fall'),each=3), c("12","01","02","03","04","05","06","07","08","09","10","11"))
+
 Oceanic_Ray <- get_inat_obs(query = "Oceanic Manta Ray",maxresults = 1000) %>% 
   select(scientific_name, time_observed_at, latitude, longitude) %>% 
   filter(scientific_name == "Mobula birostris",time_observed_at != "") %>% 
   mutate(
     year = substr(time_observed_at,0,4),
     month = substr(time_observed_at,6,7),
-    day = substr(time_observed_at,9,10)) %>% 
+    day = substr(time_observed_at,9,10), 
+    season = (indx[as.character(month)]))%>% 
   print()
 
 Reef_Ray <- get_inat_obs(query = "Reef Manta Ray",maxresults = 1000) %>% 
@@ -20,22 +24,39 @@ Reef_Ray <- get_inat_obs(query = "Reef Manta Ray",maxresults = 1000) %>%
     season = (indx[as.character(month)]))%>% 
   print()
 
-Combined_Table <- rbind(Oceanic_Ray,Reef_Ray) %>% 
-  select(scientific_name, latitude, longitude, year, month,day) %>% 
+world <- map_data("world")
+
+world_australia <- map_data("world") %>% 
+  filter(long >= 50,
+         lat <= 25,
+         lat >= -50)
+
+Reef_Ray_australia <- Reef_Ray %>% 
+  filter(longitude >= 50,
+         latitude <= 25,
+         latitude >= -50) %>% 
   print()
 
-world <- map_data("world")
+ggplot() +
+  geom_map(
+    data = world_australia, map = world_australia,
+    aes(long, lat, map_id = region)
+  ) +
+  geom_point(data = Reef_Ray_australia,
+    mapping = aes(x = longitude, y = latitude,
+      color = season,
+      size = ".1",
+      alpha = ".1"))+
+  facet_wrap(~season,ncol = 2)
 
 ggplot() +
   geom_map(
     data = world, map = world,
     aes(long, lat, map_id = region)
   ) +
-  geom_point(data = Reef_Ray,
-    mapping = aes(x = longitude, y = latitude,
-      color = season,
-      size = ".01",
-      alpha = .01))
-
-indx <- setNames( rep(c('winter', 'spring', 'summer',
-                        'fall'),each=3), c("12","01","02","03","04","05","06","07","08","09","10","11"))
+  geom_point(data = Oceanic_Ray,
+             mapping = aes(x = longitude, y = latitude,
+                           color = season,
+                           size = ".1",
+                           alpha = ".1"))+
+  facet_wrap(~season,ncol = 2)
